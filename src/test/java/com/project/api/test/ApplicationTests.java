2e235a6f393cbc12eb3e26db34ab9613c9f4f2ff
Egemen <egemenozkan@gmail.com>
2018-09-20 18:01:38 +0300

    retfb

--- src/test/java/com/project/api/test/ApplicationTests.java
+++ src/test/java/com/project/api/test/ApplicationTests.java
@@ -17,6 +17,11 @@ import org.springframework.security.oauth2.client.token.grant.client.ClientCrede
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.google.gson.Gson;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Parameter;
+import com.restfb.Version;
+import com.restfb.types.Place;
+import com.restfb.types.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,16 +34,27 @@ public class ApplicationTests {
 //    private static final String AUTH_SERVER_URL = "http://authserver:8090";
 //    private static final String TOKEN_PATH = "/oauth/token";
 //    private static final String AUTHORIZE_PATH = "/oauth/authorize";
-//    private static final String CC_CLIENT_ID  = "trusted-app";
-//    private static final String CLIENT_SECRET = "secret";
+    private static final String CC_CLIENT_ID  = "748547722151955";
+    private static final String CLIENT_SECRET = "0906127b57b84d3e7e79ad609e33777c";
 //    private static final String SCOPE = "read";
 //    
     
 	@Test
 	public void contextLoads() {
+		DefaultFacebookClient facebookClient = new DefaultFacebookClient(Version.VERSION_3_1);
+		facebookClient.obtainAppAccessToken(CC_CLIENT_ID, CLIENT_SECRET);
+		try {
+			Place place = facebookClient.fetchObject("search",Place.class,
+					Parameter.with("q", "Crystal"),
+					Parameter.with("type", "place"));
+			logger.warn(gson.toJson(place));
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
-	@Test
+	//@Test
 	public void clientCredentials() {
 		ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
         resourceDetails.setAccessTokenUri(TestConstants.AUTH_SERVER_URL + TestConstants.TOKEN_PATH);
