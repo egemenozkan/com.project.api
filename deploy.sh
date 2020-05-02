d7e553a26e2f2642508a91c173e38ce7408fdfcb
Egemen <egemenozkan@gmail.com>
2020-05-02 15:34:27 +0300

    new server

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ mvn -f $HOME/devel/workspace/projects/com.project.slugify/ clean install
 
 mvn clean install package -DskipTests -Pprod
 
-scp target/api-alfa.jar root@178.62.32.47:/opt/www/api
+scp target/api-alfa.jar root@94.237.94.131:/var/www/jee_projects
